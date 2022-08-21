5243250a81b6e0f477ec3028b5bca82a1190ce3c
Mariam Zhamakochyan <mariamzhamakochyan@gmail.com>
2022-08-21 13:36:52 +0400

    Update sub.py

--- sub.py
+++ sub.py
@@ -4,11 +4,14 @@ def sub(list1, list2):
     i = len(list1) - 1
     j = len(new_list2) - 1
     while i >= 0 and j >= 0:
-        list.append(str(list1[i]) + str(new_list2[j]))
+        list.append(str(list1[i] + ew_list2[j]))
         i -=1
-        j -=1
+        j -=1     
+    for n in range(0, len(list)):
+        if list[n] < 0:
+           list[n] *= (-1)    
     list = list[::-1]
     return str(list)
-list1 = [342,522,950]
+list1 = [342,222,950]
 list2 = [134,592,386]
 print(sub(list1, list2))
