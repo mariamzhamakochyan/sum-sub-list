0c2fde58d359d9943cc7221066e05ad831991b35
Mariam Zhamakochyan <mariamzhamakochyan@gmail.com>
2022-08-21 13:26:09 +0400

    Update sub.py

--- sub.py
+++ sub.py
@@ -4,14 +4,9 @@ def sub(list1, list2):
     i = len(list1) - 1
     j = len(new_list2) - 1
     while i >= 0 and j >= 0:
-        list.append(list1[i] + new_list2[j])
+        list.append(str(list1[i]) + str(new_list2[j]))
         i -=1
         j -=1
-    k = 0
-    while k < len(list):
-        if len(str(list[k])) != 3:
-            str(list[k]).zfill(3)
-            k += 1
     list = list[::-1]
     return str(list)
 list1 = [342,522,950]
